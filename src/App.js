10ef70274c82d1f25c6ddcb218794622ca8f5463
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-10 19:25:13 -0400

    got api to display in console

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import axios from 'axios'
 
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+      data: []
+    }
+  }
+  componentDidMount() {
+ 
+
+    axios.get("http://myapi-profstream.herokuapp.com/api/fc5aa5/wines")
+    .then(response => {
+      const wines = response.data;
+      console.log("wines ---->", wines)
+      
+      this.setState({data: wines})
+
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
+
+  }
+
+
+
   render() {
     return (
       <div>
-        
+        <h1>{"ss"+this.state.data}</h1>
       </div>
     )
   }
