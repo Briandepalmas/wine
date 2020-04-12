bd6efdeff6df2e499067e142ac802cf853ad7e83
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-12 17:57:41 -0400

    ateempted post

--- src/App.js
+++ src/App.js
@@ -22,15 +22,39 @@ export class App extends Component {
       price:[]
 
     }
+    // this.handleSubmit=this.handleSubmit.bind(this)
+    // this.addWine=this.addWine.bind(this)
+  
+  
   }
-  componentDidMount() {
+  
  
+ 
+     callApi(){
+      function addWine(event){
+        console.log("im in======")
+       let newWine={names:event.target.name,
+       year:event.target.year,
+       grapes:event.target.grapes,
+       country: event.target.country,
+       region: event.target.region,
+       description:event.target.description,
+       picture:event.target.picture,
+       price:event.target.price} 
+        return newWine;
+       
+         }
+     
+         function handleSubmit(event){
+           console.log("did update")
+             let a=addWine()
+             axios.post("http://myapi-profstream.herokuapp.com/api/fc5aa5/wines",a)
+             event.preventDefault()
+             this.callApi()
+         }
 
     axios.get("http://myapi-profstream.herokuapp.com/api/fc5aa5/wines")
     
-    
-    
-
     .then(response => {
       const wines = response.data;
       let wineNames=[]
@@ -46,7 +70,6 @@ export class App extends Component {
        this.setState({data: wines})
         
        for(let i=0;i<wines.length;i++){
-          //console.log(wines[i].name)
          wineNames.push(wines[i].name)
          wineYear.push(wines[i].year)
          wineGrapes.push(wines[i].grapes)
@@ -55,7 +78,8 @@ export class App extends Component {
          wineDescription.push(wines[i].description)
          winePicture.push(wines[i].picture)
          winePrice.push(wines[i].price)
-          this.setState({
+          
+         this.setState({
             names:wineNames,
             year:wineYear,
             grapes:wineGrapes,
@@ -67,56 +91,75 @@ export class App extends Component {
 
           }) 
        }
-      //  console.log("aaaaaaa ---->", this.state.names)
-      //  console.log("bbbbbbbbb",this.state.year)
-      //  console.log("bbbbbbbbb",this.state.grapes)
-        console.log("bbbbbbbbb",this.state.country)
-      //  console.log("bbbbbbbbb",this.state.region)
-      //  console.log("bbbbbbbbb",this.state.description)
-      //  console.log("bbbbbbbbb",this.state.picture)
-       console.log("bbbbbbbbb",this.state.price)
-      //  console.log("bbbbbbbbb",this.state.year)
+      
     })
     .catch(error => {
       console.log('there is an eror', error)
     })
-
+  }
+    componentDidMount() {
+      
+      this.callApi()
+      
+     
   }
 
-
-
   render() {
-    //console.log("la cienxzia"+this.state.data.country)
+    
     function linkID(id){
       let a=0+id
       let b="/"+a
       return b
     }
-  
+    
     let links= this.state.data.map((wine, id) => 
-  <div className="abc" key={id}>
-    <Link to={linkID(id)}>
-     <img  src={wine.picture} alt="wines"/> 
-    </Link>
-   <div>
-     <Link  id="fonts"to={linkID(id)}>{wine.name}</Link> 
-   </div>
-   
-  </div>
+        <div className="abc" key={id}>
+          
+          <Link to={linkID(id)}>
+          <img  src={wine.picture} alt="wines"/> 
+          </Link>
+          
+          <div>
+            <Link  id="fonts"to={linkID(id)}>{wine.name}</Link> 
+          </div>
+       </div>
  )
   
-  
-  // let years=this.state.year.map(wineyear => <h1>{wineyear}</h1>)
-   
-   
-   
     return (
-    <Router>
-      <div> 
       
-        <nav >
-              {links}
-        </nav>  
+    <Router> 
+      <div>
+        
+       <div>
+         <form  onSubmit={this.handleChange}>
+          <h1>ADD WINE</h1>
+          <label className="forms">
+            <h3>Name:</h3>
+            <input type="text" placeholder="Wine" onChange={this.addWine}></input>
+            <h3> Year:</h3>
+            <input type="text" placeholder="Year" onChange={this.addWine}></input>
+            <h3>Grape:</h3>
+            <input type="text" placeholder="Grape" onChange={this.addWine}></input>
+            <h3>Country:</h3>
+            <input type="text" placeholder="country" onChange={this.addWine}></input>
+            <h3>Region:</h3>
+            <input type="text" placeholder="region" onChange={this.addWine}></input>
+            <h3>Description:</h3>
+            <input type="text" placeholder="description" onChange={this.addWine}></input>
+            <h3>Picture:</h3>
+            <input type="text" placeholder="picture" onChange={this.addWine}></input>
+            <h3>Price:</h3>
+            <input type="text" placeholder="price" onChange={this.addWine}></input>
+           </label>
+          <input type="submit" value="Submit"onClick={this.addWine}></input>
+        </form>
+        <div>
+             <nav >
+             <button id="button">{links}</button>  
+             </nav>
+        </div>
+       </div>
+      
        </div>
     </Router>
     )
